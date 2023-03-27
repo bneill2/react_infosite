fd8adc81f8b1c49f4ce7cf2502b6c4974b9db18c
Bryan <bryan@rendia.com>
2023-03-27 19:04:31 -0400

    final project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import react from 'React'
+import logo from '../assets/logo192.png'
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <h3 className="nav--header">
+                <img src={logo} className="nav--header-img" alt="react-logo" />
+                ReactFacts
+            </h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </div>
+    )
+};
