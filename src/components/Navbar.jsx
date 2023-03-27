40419756eb9bb0e210963caea439e59e08ba8e92
Bryan <bryan@rendia.com>
2023-03-27 19:29:50 -0400

    react has to be lowercase?

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import React from 'React'
+import React from 'react'
+import logo from '../assets/logo192.png'
 
 export default function Navbar() {
     return (
         <div className="navbar">
             <h3 className="nav--header">
-                {/* <img src={logo} className="nav--header-img" alt="react-logo" />
-                ReactFacts */}
+                <img src={logo} className="nav--header-img" alt="react-logo" />
+                ReactFacts
             </h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </div>
